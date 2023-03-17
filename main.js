6cddbf6c7c23b770aadfe7dd0197345262626da7
Michal Rutz <michal.rutz@gmail.com>
2023-03-17 16:13:39 +0100

    realistic light

--- main.js
+++ main.js
@@ -9,23 +9,22 @@ const sizes = {
 
 //CAMERA
 const camera = new THREE.PerspectiveCamera( 
-  15 , sizes.w/sizes.h, 1, 100
+  15 , sizes.w/sizes.h, 1, 150
 )
 
 camera.position.set( 0, 0 , 3 )
 //light
-const spot = new THREE.SpotLight("red", 0.8, 20, 90 )
+const spot = new THREE.SpotLight("red", 4, 20, 90 )
 spot.position.set(-1.5,0,-1)
-spot.decay = 10 
 scene.add(spot);
-const spot2 = new THREE.SpotLight("blue", 0.8, 20, 45 )
+const spot2 = new THREE.SpotLight("blue", 8, 20, 45 )
 spot2.position.set(2,0,-1)
 scene.add(spot2);
-let spotWhite = new THREE.SpotLight("white", 0.9, 20, 90 )
+let spotWhite = new THREE.SpotLight("white", 4, 20, 90 )
 spotWhite.position.set(2,0,1)
 scene.add(spotWhite);
 
-const ambient = new THREE.AmbientLight( "blue", 0.02 )
+const ambient = new THREE.AmbientLight( "blue", 0.1 )
 scene.add(ambient)
 
 //MESH
@@ -60,7 +59,7 @@ loader.load('./static/disco draco.glb', function(gltf) {
   let timeElapsed = Date.now()/1000 - start
   //model.rotation.y = timeElapsed/100
 
-  rendere.render( scene , camera )
+  renderer.render( scene , camera )
   window.requestAnimationFrame(animate)
 
 }
@@ -72,9 +71,10 @@ animate()
 scene.add(camera)
 
 const canvas = document.getElementById("canvas_1")
-const rendere = new THREE.WebGLRenderer({canvas:canvas, antialias:true })
-rendere.setSize( sizes.w, sizes.h )  
-rendere.render( scene , camera )
+const renderer = new THREE.WebGLRenderer({canvas:canvas, antialias:true })
+renderer.setSize( sizes.w, sizes.h ) 
+renderer.physicallyCorrectLights = true  
+renderer.render( scene , camera )
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 const controls = new OrbitControls( camera, canvas )
@@ -87,11 +87,11 @@ function animate () {
   spot.position.y = (Math.sin(timeElapsed))
   spot.position.z = (Math.cos(timeElapsed))
   
-
+  spotWhite.intensity = (Math.sin(Math.max(timeElapsed/4, 0.2)))*10
 
   controls.update()
 
-  rendere.render( scene , camera )
+  renderer.render( scene , camera )
   window.requestAnimationFrame(animate)
 }
 animate()
@@ -102,14 +102,14 @@ function resize() {
   camera.aspect = sizes.w / sizes.h 
   camera.updateProjectionMatrix()
   if (window.innerWidth<sizes.w){
-    rendere.setSize( window.innerWidth, window.innerWidth )
+    renderer.setSize( window.innerWidth, window.innerWidth )
   }
   else {
-    rendere.setSize( sizes.w, sizes.h )
+    renderer.setSize( sizes.w, sizes.h )
 
   }
-  rendere.setPixelRatio( 3 )
-  rendere.render( scene , carotationmera )
+  renderer.setPixelRatio( 3 )
+  renderer.render( scene , carotationmera )
 }
 
 const rotateMesh = (model, rotation) => {
